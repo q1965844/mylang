e5d9e1875c64c8e420300c31a233ac594f2b1b9e
陳敬旻／ジミ <q1965844@gms.nutc.edu.tw>
2019-07-10 19:42:31 +0900

    Tokenizer まだ終わっていない

--- MyLang/SpaceSeparatedTokenizer.cs
+++ MyLang/SpaceSeparatedTokenizer.cs
@@ -7,19 +7,29 @@ using System.Text.RegularExpressions;
 namespace MyLang
 {
     /// <summary>
-    /// 単純なトークナイザ
+    /// 単純なトークナイザ(tokenizer)
     /// 
     /// トークンは、必ず一つ以上のスペースで区切られている必要がある
     /// </summary>
     class SpaceSeparatedTokenizer : ITokenizer
     {
+        static Regex regexNumber = new Regex(@"^\d$");
+        static Regex regexSpace = new Regex(@"\s+");
+
         public SpaceSeparatedTokenizer()
         {
 
         }
 
+        Token match(string s)
+        {
+            return new Token(TokenType.Number, "1")); ;
+        }
+
         public IList<Token> Tokenize(string src)
         {
+            var tokenString = regexSpace.Split(src);
+            return tokenString.Select(x =>match(x));
             // TODO: 仮のダミー実装
             var dummy = new List<Token>();
             dummy.Add(new Token(TokenType.Number, "1"));

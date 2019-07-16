2338e4a6715b94dccf03c2f3db3692de1b3f1034
陳敬旻／ジミ <q1965844@gms.nutc.edu.tw>
2019-07-16 20:02:34 +0900

    Interpreter 実装完了しました。

--- MyLang/Interpreter.cs
+++ MyLang/Interpreter.cs
@@ -13,9 +13,39 @@ namespace MyLang
 
         public float Run(Ast.Ast ast)
         {
-            // TODO: 仮のダミー実装
-            return 0;
+            return calculate((Exp)ast);
         }
 
+        float calculate(Exp exp)
+        {
+            if (exp is BinOp)
+            {
+                var bin = (BinOp)exp;
+                var lValue = calculate(bin.Lhs);
+                var rValue = calculate(bin.Rhs);
+                switch (bin.Operator)
+                {
+                    case BinOpType.Add:
+                        return lValue + rValue;
+                    case BinOpType.Sub:
+                        return lValue - rValue;
+                    case BinOpType.Multiply:
+                        return lValue * rValue;
+                    case BinOpType.Divide:
+                        return lValue / rValue;
+                    default:
+                        throw new Exception("bug");
+                }
+            }
+            else if (exp is Number)
+            {
+                var num = (Number)exp;
+                return num.Value;
+            }
+            else
+            {
+                throw new Exception("bug");
+            }
+        }
     }
 }
